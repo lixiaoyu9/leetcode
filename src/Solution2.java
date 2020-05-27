42436af72591ba6717eec5d4f1887f706e5a5d14
李晓宇 <xiaoyu.lxy@120yibao.com>
2020-05-27 18:32:22 +0800

    两数相加

--- src/Solution2.java
+++ src/Solution2.java
@@ -13,12 +13,38 @@
 // Related Topics 链表 数学
 public class Solution2 {
     public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
 
+        ListNode l2 = new ListNode(5);
+        ListNode l21 = new ListNode(6);
+        l2.next = l21;
+        l21.next = new ListNode(4);
+        ListNode curr = addTwoNumbers(l1, l2);
+        while (curr != null) {
+            System.out.println(curr.val);
+            curr = curr.next;
+        }
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
-        return null;
+        ListNode pre = new ListNode(0);
+        ListNode curr = pre;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+            if (carry > 0) {
+                curr.next = new ListNode(carry);
+            }
+        }
+        return pre.next;
     }
 
     public static class ListNode {
